b508d7faaec132cd46cd139c021eb097262a9cd0
Rens <rm.aarts@student.avans.nl>
2026-03-24 11:41:10 +0100

    Fix formatting in node_types.php by adding trailing commas

--- config/node_types.php
+++ config/node_types.php
@@ -5,30 +5,30 @@ return [
         'type' => 'rectangle',
         'name' => 'Rechthoek',
         'icon' => 'square',
-        'aruco' => '1'
+        'aruco' => '1',
     ],
     [
         'type' => 'server',
         'name' => 'Server',
         'icon' => 'server',
-        'aruco' => '2'
+        'aruco' => '2',
     ],
     [
         'type' => 'database',
         'name' => 'Database',
         'icon' => 'database',
-        'aruco' => '3'
+        'aruco' => '3',
     ],
     [
         'type' => 'application',
         'name' => 'Applicatie',
         'icon' => 'layout-dashboard',
-        'aruco' => '4'
+        'aruco' => '4',
     ],
     [
         'type' => 'user',
         'name' => 'Gebruiker',
         'icon' => 'user',
-        'aruco' => '5'
+        'aruco' => '5',
     ],
 ];
