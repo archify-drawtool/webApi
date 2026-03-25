50b9bed1fb7e4ecc0c080aa14245582815c5e9c6
Rens <rm.aarts@student.avans.nl>
2026-03-25 12:21:43 +0100

    Routes inside auth middleware

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,10 @@ Route::get('/health', function () {
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/node-types', [NodeTypeController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/node-types', [NodeTypeController::class, 'index']);
 });
