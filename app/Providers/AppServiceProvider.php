1b018f9a1258b83511e7e150bb7a262ac52c209f
Daan Smit <daan.smit@kpnmail.nl>
2026-04-11 21:06:44 +0200

    Added custom metrics

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\PersonalAccessToken;
 use Spatie\Prometheus\Facades\Prometheus;
 
 class AppServiceProvider extends ServiceProvider
@@ -34,7 +35,7 @@ class AppServiceProvider extends ServiceProvider
         // Actieve tokens
         Prometheus::addGauge('active_tokens_total')
             ->helpText('Aantal actieve sanctum tokens')
-            ->value(fn() => \Laravel\Sanctum\PersonalAccessToken::count());
+            ->value(fn () => PersonalAccessToken::count());
 
         // Inlogpogingen — wordt verhoogd in AuthController
         Prometheus::addCounter('login_attempts_total')
