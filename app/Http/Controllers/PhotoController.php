8d30774d658969de73d2858192ef27c2036eb619
Freekvrijn <freekrijn@gmail.com>
2026-04-16 12:29:32 +0200

    All users now have acces to all projects
    Removed check if project is owned by user

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Project;
 use App\Services\PhotoService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-use Illuminate\Validation\ValidationException;
 
 class PhotoController extends Controller
 {
@@ -25,17 +23,6 @@ class PhotoController extends Controller
         ]);
 
         $projectId = $request->integer('project_id');
-
-        $ownsProject = Project::where('id', $projectId)
-            ->where('created_by', $request->user()->id)
-            ->exists();
-
-        if (! $ownsProject) {
-            throw ValidationException::withMessages([
-                'project_id' => ['Je kunt geen foto uploaden naar een project dat niet van jou is.'],
-            ]);
-        }
-
         $path = $this->photoService->store($request->file('photo'), $projectId);
 
         return response()->json([
